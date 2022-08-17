6d8f213a63a6d71c4c54a7ff3aacd050ceedd04f
whitney <whitney.njogu@student.moringaschool.com>
2022-08-17 10:27:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+let companyName = 'Scuber';
+//const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+console.log (mostProfitableNeighborhood)
+
+let companyCeo = 'Susan Smith';
+console.log (companyCeo)
